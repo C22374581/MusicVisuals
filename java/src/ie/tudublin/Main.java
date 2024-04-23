15ce4c74d3e5f1d0de426ddde4c38b2e5a6d9c8c
Tadhg Roche <C22348761@mytudublin.ie>
2024-04-23 19:23:04 +0100

    added snow song

--- java/src/ie/tudublin/Main.java
+++ java/src/ie/tudublin/Main.java
@@ -2,7 +2,6 @@ package ie.tudublin;
 
 
 import example.Project;
-import example.Project2;
 import example.RotatingAudioBands;
 
 
@@ -10,7 +9,7 @@ public class Main {
 
     public void startUI() {
         String[] a = { "MAIN" };
-        processing.core.PApplet.runSketch(a, new Project2());
+        processing.core.PApplet.runSketch(a, new Project());
     }
 
     public static void main(String[] args) {
