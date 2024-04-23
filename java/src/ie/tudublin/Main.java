dedb8ffd52316d860e855568d3bb22fe25e4ce2c
C22374581 <C22374581@mytudublin.ie>
2024-04-23 20:29:42 +0100

    Fix sketch initialization in Main.java

--- java/src/ie/tudublin/Main.java
+++ java/src/ie/tudublin/Main.java
@@ -10,7 +10,7 @@ public class Main {
 
     public void startUI() {
         String[] a = { "MAIN" };
-        processing.core.PApplet.runSketch(a, new Project2());
+        processing.core.PApplet.runSketch (a, new Project());
     }
 
     public static void main(String[] args) {
